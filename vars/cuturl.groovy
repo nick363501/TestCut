d559f67b27c018e3c71359bb0f07357da463a9e0
Bob.Nicolais <bob.x.nicolais@kp.org>
2020-10-22 15:49:49 -0400

    Modified Shared

--- vars/cuturl.groovy
+++ vars/cuturl.groovy
@@ -1,9 +1,4 @@
-//{    def jenkinsUrl = sh (
-//        "echo ${env.BUILD_URL} | rev | cut -d '/' -f3- | rev"
-//        returnStdout: true
-//    ).trim()
-//}
-def call(String name = 'human') {​​
+def call() {​​
     // Any valid steps can be called from this code, just like in other// Scripted Pipeline
-    echo "Hello, ${​​name}​​."
-}​​
+    echo "Hello,"
+}​​ß

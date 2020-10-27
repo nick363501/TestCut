b54833ebd939d84b5fd237758269628a09c45d0e
Bob.Nicolais <bob.x.nicolais@kp.org>
2020-10-27 09:28:51 -0400

    Modified Stuff

--- vars/cuturl.groovy
+++ vars/cuturl.groovy
@@ -1,6 +1,5 @@
 def call() {​​
     // Any valid steps can be called from this code, just like in other// Scripted Pipeline
-    def jenkinsUrl="${JOB_URL}"
-    println jenkinsUrl
+    println "${BUILD_URL}"
     
 }​​
